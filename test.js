29087ca746a3660d8a031bf1c53102cfe921433e
Julian Gruber <julian@juliangruber.com>
2017-08-28 17:50:38 +0200

    add passing row height test

--- test.js
+++ test.js
@@ -4,13 +4,27 @@ const test = require('tape')
 const Unendlich = require('.')
 
 test('Unendlich', t => {
-  t.test('new Unendlich({ rows, inner, outer, render })', t => {
+  t.test('no rows', t => {
     const rows = []
     const inner = document.createElement('div')
     const outer = document.createElement('div')
     const render = () => {}
     const view = new Unendlich({ rows, inner, outer, render })
     t.ok(view)
+    t.end()
+  })
+  t.test('row height', t => {
+    const rows = [{}]
+    const inner = document.createElement('div')
+    const outer = document.createElement('div')
+    const render = () => {
+      const el = document.createElement('div')
+      el.style.height = '13px'
+      return el
+    }
+    document.body.appendChild(inner)
+    const view = new Unendlich({ rows, inner, outer, render })
+    t.equal(view.rowHeight, 13)
     t.end()
   })
   t.end()
