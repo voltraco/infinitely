bbcbc7fba01a0bc1761f06839cb0dd918d820ee1
Julian Gruber <julian@juliangruber.com>
2017-08-28 17:59:49 +0200

    add passing num pages test

--- test.js
+++ test.js
@@ -13,7 +13,7 @@ test('Unendlich', t => {
     t.ok(view)
     t.end()
   })
-  t.test('row height', t => {
+  t.test('.rowHeight', t => {
     const rows = [{}]
     const inner = document.createElement('div')
     const outer = document.createElement('div')
@@ -25,6 +25,24 @@ test('Unendlich', t => {
     document.body.appendChild(inner)
     const view = new Unendlich({ rows, inner, outer, render })
     t.equal(view.rowHeight, 13)
+    t.end()
+  })
+  t.test('.numPages', t => {
+    let rows = Array(123)
+    const inner = document.createElement('div')
+    const outer = document.createElement('div')
+    const render = () => {
+      const el = document.createElement('div')
+      el.style.height = '13px'
+      return el
+    }
+    const page = 100
+    document.body.appendChild(inner)
+    const view = new Unendlich({ rows, inner, outer, render, page })
+    t.equal(view.numPages, Math.ceil(rows.length / page))
+    rows = Array(23)
+    view.setRows(rows)
+    t.equal(view.numPages, Math.ceil(rows.length / page))
     t.end()
   })
   t.end()
